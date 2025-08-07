fdcc7add037db28685d0a02cdc1a06ddb4809957
Ozi <ozisadiqkasai@gmail.com>
2025-08-07 13:33:23 +0100

    figuring api

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 // src/apijs
 import axios from 'axios';
 
-const instance = axios.create({
+const api = axios.create({
   baseURL:  import.meta.env.VITE_BACKEND_URL ||'http://localhost:3000/api', // change to your backend URL
 });
 
-export default instance;
+export default api;
